431c6a9de6d3165ce65b376d3d156e3c86fe309f
Maeva Loo <124236401+changjingloo@users.noreply.github.com>
2023-02-02 17:51:52 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 import csv
 def profitloss_function():
+    '''
+    This functions runs everything related to "profitloss"
+    which includes the return value.
+    '''
   
 
