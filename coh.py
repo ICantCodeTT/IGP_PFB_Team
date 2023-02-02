0864c970dc135e37bea4fa7bba00f91c0aee7652
JH <124236738+All-namestaken@users.noreply.github.com>
2023-02-02 18:05:59 +0800

    Update coh.py

--- coh.py
+++ coh.py
@@ -12,17 +12,21 @@ def coh_function():
 # read the csv file to append profit and quantity from the csv.
     with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         reader = csv.reader(file)
-        next(reader) # skip header
-
-        cashonhand = 0 # Creation of needed variables
+        # skip header
+        next(reader) 
+        
+        # Creation of needed variables
+        cashonhand = 0 
         row_counter = 0
         check = 0
         data_to_return = []
      for row in reader:
             row_counter += 1
-            if int(row[1]) > cashonhand: # If int in row[1] is greater than cashonhand, it equals to cashonhand
+            # If int in row[1] is greater than cashonhand, it equals to cashonhand
+            if int(row[1]) > cashonhand: 
                 cashonhand = int(row[1])
-                check += 1 # check by counting if all the rows is greater than cashonhand
+                # check by counting if all the rows is greater than cashonhand
+                check += 1 
             else: 
                 # Append all the days that are lower than the previous days
                 data_to_return.append(f"[CASH DEFECIT] DAY: {row[0]}, AMOUNT: USD{cashonhand - int(row[1])}")
