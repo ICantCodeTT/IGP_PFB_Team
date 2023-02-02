7067df34b8b62bd2cb2a2bb27b6c98b5d41d7e27
CeleneChong <124236592+CeleneChong@users.noreply.github.com>
2023-02-02 17:53:58 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -5,5 +5,17 @@ def profitloss_function():
     This functions runs everything related to "profitloss"
     which includes the return value.
     '''
-  
+# create a file to csv file
+    fp = Path(r"C:\IGP_PFB_TeamA\csv_reports\profit-and-loss-usd (Day 48 to 52).csv")
+
+# read the csv file to append profit and quantity from the csv.
+    with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+        reader = csv.reader(file)
+        next(reader) # skip header
+
+
+        profitloss = 0 # Creation of needed variables 
+        row_counter = 0
+        check = 0
+        data_to_return = []
 
