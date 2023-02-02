c7cc356ad4e4b73c01bdaf70d2a7079adca7b3ae
Maeva Loo <124236401+changjingloo@users.noreply.github.com>
2023-02-02 17:57:06 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -18,4 +18,19 @@ def profitloss_function():
         row_counter = 0
         check = 0
         data_to_return = []
+        
+        for row in reader:
+            row_counter += 1 
+            if int(row[4]) > profitloss: # If int in row[4] is greater than profitloss, it equals to profitloss
+                profitloss = int(row[4])
+                check += 1 # check by counting if all the rows is greater than profitloss
+            else:
+                # Append all the days that are lower than the previous days
+                data_to_return.append(f"[PROFIT DEFECIT] DAY: {row[0]}, AMOUNT: USD{profitloss - int(row[4])}")
+                profitloss = int(row[4])
+        # Only if check and row_counter have the exact final value, it will append "NET PROFIT SURPLUS" statement
+        if check == row_counter: 
+            data_to_return.append("[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+    return data_to_return
+
 
